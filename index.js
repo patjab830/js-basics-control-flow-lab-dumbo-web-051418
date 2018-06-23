aba1c1fe01a0922d63d854d885a70befeea6c029
github username <github email address>
2018-06-23 12:56:55 -0400

    implemented conditional flow

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  } else if (someValue > 2500) {
+    return 'No can do.';
+  } else if ( someValue > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
+}
+
+
+function ternaryCheckCity(city) {
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(amount) {
+  switch (amount) {
+    case "generous":
+      return "Thank you so much."
+    case "not as generous":
+      return "Thank you."
+    default:
+      return "Bye."
+  }
+}
